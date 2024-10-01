17e287499c240c5b56b9f288c88c73a2b6d116f7
Perlaphim <Xatzigeorgioupavlos@gmail.com>
2024-10-01 23:38:39 +0300

    jaxrs-hiber

---
+++ src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
@@ -0,0 +1,30 @@
+package gr.aueb.cf.schoolapp.mapper;
+
+import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherUpdateDTO;
+import gr.aueb.cf.schoolapp.model.Teacher;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Mapper {
+
+    private Mapper() {}
+
+    public static Teacher mapToTeacher(TeacherInsertDTO dto) {
+        return new Teacher(null, dto.getVat(), dto.getFirstname(), dto.getLastname());
+    }
+
+    public static Teacher mapToTeacher(TeacherUpdateDTO dto) {
+        return new Teacher(dto.getId(), dto.getVat(), dto.getFirstname(), dto.getLastname());
+    }
+
+    public static TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
+        return new TeacherReadOnlyDTO(teacher.getId(), teacher.getVat(), teacher.getFirstname(), teacher.getLastname());
+    }
+
+    public static List<TeacherReadOnlyDTO> teachersToReadOnlyDTOs(List<Teacher> teachers) {
+        return teachers.stream().map(Mapper::mapToTeacherReadOnlyDTO).collect(Collectors.toList());
+    }
+}
